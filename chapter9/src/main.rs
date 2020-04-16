4ef22d2634242a52852cfa8908cb288eeda0dd79
benzhai <ben.zhai@grapecity.com>
2020-04-16 18:07:16 +0800

    416

--- chapter9/src/main.rs
+++ chapter9/src/main.rs
@@ -1,7 +1,25 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
 fn main() {
     test1();
+    test2();
 }
 
 fn test1(){
-    panic!("crash");
+    //panic!("crash");
+}
+
+fn test2(){
+    let f = File::open("hello.text");
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.text"){
+                Ok(file) => file,
+                Err(error) => panic!("Problem creating the file: {:?}", error),
+            },
+            other_error => panic!("Problem opening the file: {:?}", other_error)
+        },
+    };
 }
